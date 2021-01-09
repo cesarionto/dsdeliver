f8d6022d21ce01c7d985d822ef16d9e936b1a37a
cesarionto <cesariopereiraneto@gmail.com>
2021-01-09 14:22:28 -0300

    Project created

---
+++ backend/src/main/java/com/example/demo/DsdeliveryapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliveryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliveryapiApplication.class, args);
+	}
+
+}
